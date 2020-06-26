7db71149df9e53a456b51df732fb78cbf126cab8
suraj790 <66895098+suraj790@users.noreply.github.com>
2020-06-26 23:41:34 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 	error_reporting(E_ERROR | E_PARSE);      //optional,to remove warning messages.
-     $conn=mysqli_connect("localhost","root","","ecom");
+     $conn=mysqli_connect("","aumnpycygh","KTq3FsVs2B","ecom");
         if ($conn)
           {
            echo "database connected with ecom","<br>";
